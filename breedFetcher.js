544ccda29a607895cd746ab98442dbbae55417da
Mrdo Mrdo <mayilyane9@gmail.com>
2021-08-26 15:20:34 -0700

    Commit index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,9 +2,14 @@ const request = require('request');
 const breed = process.argv[2];
 const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
-request(url, function(error, response, body) {
-  console.error('error:', error);
-  console.log('statusCode:', response && response.statusCode);
-  const data = JSON.parse(body);
-  console.log('description:', data[0].description);
-});
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(url, function(error, response, body) {
+    console.error('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+    const data = JSON.parse(body);
+    console.log('description:', data[0].description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
